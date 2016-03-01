b106d2d68822735d4f06e85ac90eae7fd598021d
Andrew Chen <sixpercentninja@Andrews-MacBook-Pro-2.local>
2016-03-01 03:56:21 -0800

    storyscene implementation

---
+++ ThreadMagic/StoryScene.swift
@@ -0,0 +1,60 @@
+//
+//  StoryScene.swift
+//  ThreadMagic
+//
+//  Created by Andrew Chen on 2/29/16.
+//  Copyright © 2016 Andrew Chen. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class StoryScene: SKScene {
+    
+    var mc: Player!
+    
+    override func didMoveToView(view: SKView) {
+        
+        SKTAudio.sharedInstance().playBackgroundMusic("Remember.mp3")
+        
+        let bgImage = SKSpriteNode(imageNamed: "Parchment")
+        
+        bgImage.position = CGPointMake(self.size.width/2, self.size.height/2)
+        bgImage.runAction(SKAction.fadeInWithDuration(1))
+        
+        self.addChild(bgImage)
+        
+        
+        /////
+        
+        //code for story text to appear line by line
+        
+        /////
+        
+        let button1 = SKSpriteNode(imageNamed: "Continue")
+        button1.setScale(0.8)
+        button1.position = CGPoint(x: 900, y: 128)
+        button1.name = "Continue"
+        button1.runAction(SKAction.fadeInWithDuration(3))
+        button1.alpha = 0
+        
+        self.addChild(button1)
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        SKTAudio.sharedInstance().fadeVolumeAndPause()
+        let touch = touches.first
+        let location = touch!.locationInNode(self)
+        let node = self.nodeAtPoint(location)
+        
+        let scene = GameScene(size:CGSize(width: 1280, height: 800))
+        scene.mc = mc
+        if (node.name == "Continue") {
+            let transition = SKTransition.crossFadeWithDuration(2)
+            self.scene!.view?.presentScene(scene, transition: transition)
+        }
+    }
+
+}
