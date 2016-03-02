a8dc131901d3c2af31e4d020d5f94b0457d4fc74
Andrew Chen <sixpercentninja@Andrews-MacBook-Pro-2.local>
2016-03-02 14:12:31 -0800

    story scene further completed and title screen completed

--- ThreadMagic/GameScene/MainMenuScene.swift
+++ ThreadMagic/GameScene/MainMenuScene.swift
@@ -13,50 +13,79 @@ class MainMenuScene: SKScene {
     
     
     override func didMoveToView(view: SKView) {
-        //self.backgroundColor = SKColor(red: 0.15, green:0.15, blue:0.3, alpha: 1.0)
-        let button1 = SKSpriteNode(imageNamed: "StartNewGame")
-        button1.setScale(0.8)
-        button1.position = CGPoint(x: 482, y: 220)
+        
+    self.addBG("Title.png")
+    self.scaleMode = .Fill
+
+        
+        let button1 = SKSpriteNode(imageNamed: "startButton")
+        button1.setScale(0.7)
+        button1.position = CGPoint(x: 1117, y: 320)
         button1.name = "StartNewGame"
         button1.runAction(SKAction.fadeInWithDuration(1))
         button1.alpha = 0
         
-        self.addChild(button1)
-        
-        let button2 = SKSpriteNode(imageNamed: "Continue")
-        button2.setScale(0.8)
-        button2.position = CGPoint(x: 798, y: 220)
+        let button2 = SKSpriteNode(imageNamed: "continueButton")
+        button2.setScale(0.7)
+        button2.position = CGPoint(x: 1117, y: 230)
         button2.name = "Continue"
         button2.runAction(SKAction.fadeInWithDuration(1))
         button2.alpha = 0
         
-        self.addChild(button2)
-        
-        let button3 = SKSpriteNode(imageNamed: "Credit")
-        button3.setScale(0.8)
-        button3.position = CGPoint(x: 635, y: 128)
+        let button3 = SKSpriteNode(imageNamed: "creditsButton")
+        button3.setScale(0.7)
+        button3.position = CGPoint(x: 1117, y: 140)
         button3.name = "Credit"
         button3.runAction(SKAction.fadeInWithDuration(1))
         button3.alpha = 0
         
+        self.addChild(button1)
+        self.addChild(button2)
         self.addChild(button3)
         
         SKTAudio.sharedInstance().playBackgroundMusic("Good Memories.mp3")
         
+        print(button1.frame)
+        print(button2.frame)
+        print(button3.frame)
+        
     }
+    
+    func addBG(backgroundName: String) {
+        let bg = SKSpriteNode(imageNamed: backgroundName)
+        bg.name = "bg"
+        bg.zPosition = -1
+        bg.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        bg.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(bg)
+    }
+
+    
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
 
         SKTAudio.sharedInstance().fadeVolumeAndPause()
-        let touch = touches.first
-        let location = touch!.locationInNode(self)
-        let node = self.nodeAtPoint(location)
         
-        let scene = StoryScene(size:CGSize(width: 1280, height: 800))
+        let touch = touches.first
+        let positionInScene = touch!.locationInNode(self)
 
-        if (node.name == "StartNewGame") {
-            let transition = SKTransition.crossFadeWithDuration(3)
+        let touchedNode = self.nodeAtPoint(positionInScene)
+        print(positionInScene)
+        
+        
+        if let name = touchedNode.name {
+
+        if name == "StartNewGame" {
+            let transition = SKTransition.crossFadeWithDuration(1)
+            let scene = StoryScene(size:CGSize(width: 1280, height: 800))
             self.scene!.view?.presentScene(scene, transition: transition)
         }
+        
+        else if name == "Credit" {
+            let transition = SKTransition.crossFadeWithDuration(1)
+            let scene2 = CreditScene(size:CGSize(width: 1280, height: 800))
+            self.scene!.view?.presentScene(scene2, transition: transition)
+        }
     }
+}
 }
